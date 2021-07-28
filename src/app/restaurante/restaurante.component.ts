22bb5e4ccff2523550682f07b13f37bfe5fa65e5
Faculdade iv2 <contato@faculdadeiv2.com.br>
2021-07-28 15:14:13 -0300

    dia 3

---
+++ src/app/restaurante/restaurante.component.ts
@@ -0,0 +1,109 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { RestaurantesService } from '../shared/restaurantes.service';
+
+@Component({
+  selector: 'app-restaurante',
+  templateUrl: './restaurante.component.html',
+  styleUrls: ['./restaurante.component.scss']
+})
+export class RestauranteComponent implements OnInit {
+
+  mediaGeral: Array<any> = [];
+
+  ratingArr: Array<number> = [];
+  rating: number = 5;
+  starCount: number = 5;
+
+  usuario_rating: number = 3;
+  usuario_ratingArr: Array<number> = [];
+  comentarios_usuarios_ratingArr: Array<number> = [];
+
+  comentario_usuario: any;
+  comentarios_usuarios: Array<any> = [];
+
+  constructor(
+    public dialogRef: MatDialogRef<RestauranteComponent>,
+    @Inject(MAT_DIALOG_DATA) public restaurante: any,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    private _restauranteService: RestaurantesService,
+  ) { }
+
+  ngOnInit(): void {
+    for (let index = 0; index < this.starCount; index++) {
+      this.ratingArr.push(index);
+    }
+
+    for (let index = 0; index < this.starCount; index++) {
+      this.usuario_ratingArr.push(index);
+    }
+
+    for (let index = 0; index < this.starCount; index++) {
+      this.comentarios_usuarios_ratingArr.push(index);
+    }
+
+    this.listarComentarios(this.restaurante.estrelas)
+  }
+
+  showIcon(contagem: number, index: number) {
+    if (contagem >= index + 1) {
+      return 'star';
+    } else {
+      return 'star_border';
+    }
+  }
+
+  onClick(rating: number) {
+    this.usuario_rating = rating;
+    return false;
+  }
+
+  enviarComentario() {
+    this._restauranteService
+    .criaComentarioDousuario(this.restaurante.id, 'idprovisorio', {
+      comentario: this.comentario_usuario,
+      estrelas: this.usuario_rating,
+      comentadoEm: new Date(),
+      autor: {
+        nome: 'bootcamper',
+        foto: '',
+        uid: 'idprovisorio'
+      }
+    }).then(() => this.comentario_usuario = '');
+  }
+
+  listarComentarios(param: number) {
+    this.mediaGeral = [];
+    this._restauranteService.listaComentariosDoRestaurante(this.restaurante.id)
+    .subscribe(comentarios => {
+      this.comentarios_usuarios = comentarios.map(comentario => comentario);
+      comentarios.forEach(comentario => {
+        return this.mediaGeral.push(comentario.estrelas);
+      });
+      this.mediaGeral.push(param);
+      const sum = this.mediaGeral.reduce((a: number, b: number) => a + b, 0);
+      const toDisplay = Math.round(sum / this.mediaGeral.length);
+      return this.rating = toDisplay;
+    });
+  }
+
+  excluirComentario(comentario: any) {
+    if (window.confirm('Deseja mesmo excluir esse comentário?')) {
+      this._restauranteService.excluirComentario(this.restaurante.id, comentario.autor.uid);
+      this.snackBar.open('Comentário excluido com sucesso.', 'X', {
+        verticalPosition: 'top',
+        horizontalPosition: 'right',
+        duration: 5000
+      })
+    }
+  }
+
+  formataData(seconds: number) {
+    const data = new Date(seconds * 1000).toLocaleDateString();
+    const horario = new Date(seconds * 1000).toLocaleTimeString();
+    
+    return `${data} às ${horario}`;
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBPSem0i3-ab7_HWpcsV2bKE3PkZmT1jug",
+    authDomain: "findfood-faculdadeiv2.firebaseapp.com",
+    projectId: "findfood-faculdadeiv2",
+    storageBucket: "findfood-faculdadeiv2.appspot.com",
+    messagingSenderId: "678399540548",
+    appId: "1:678399540548:web:eaad3bedbda186e5e8eb11"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
