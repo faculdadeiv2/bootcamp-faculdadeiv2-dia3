092e9618573f12e391ef1fb063df42004003966f
Lucas Favareto Santos <lucasfavaretosantos98@gmail.com>
2021-07-28 18:28:53 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBPSem0i3-ab7_HWpcsV2bKE3PkZmT1jug",
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
     authDomain: "findfood-faculdadeiv2.firebaseapp.com",
     projectId: "findfood-faculdadeiv2",
     storageBucket: "findfood-faculdadeiv2.appspot.com",
     messagingSenderId: "678399540548",
-    appId: "1:678399540548:web:eaad3bedbda186e5e8eb11"
+    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   }
 };
